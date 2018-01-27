88ef88fd858f0c26a912c5fd92ea87dc81e46d25
Rewolt <rewolt90@gmail.com>
2018-01-27 17:34:21 +0100

    Add project files.

---
+++ ebookDownloader/NetDownloader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.Linq;
+
+namespace ebookDownloader
+{
+    class NetDownloader
+    {
+        private string _content;
+        private HttpClient client;
+
+        public NetDownloader()
+        {
+            client = new HttpClient();
+            client.Timeout = new TimeSpan(1, 0, 0);
+        }
+
+        public async Task<string> GetContent (Uri uri)
+        {
+            var cl = new HttpClient();
+            var response = await cl.GetAsync(uri);
+            var content = string.Empty;
+
+            var cos = response.Content.Headers.ContentType;
+            if (response.Content.Headers.ContentType.MediaType == "text/plain")
+            {
+                content = await response.Content.ReadAsStringAsync();
+            }
+            _content = content;
+            return content;
+        }
+
+        public void HttpGetFiles(Uri url, DirectoryInfo directory)
+        {
+            try
+            {
+                using (HttpResponseMessage response = client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead).Result)
+                using (Stream streamToReadFrom = response.Content.ReadAsStreamAsync().Result)
+                {
+                    var bookName = response.RequestMessage.RequestUri.Segments.Last();
+                    var realBookFileName = new FileInfo(System.Web.HttpUtility.UrlDecode(bookName));
+                    Console.Write(realBookFileName.Name + "\t");
+                    //if (!realBookFileName.Extension.Equals(".doc", StringComparison.CurrentCultureIgnoreCase) &&
+                    //    !realBookFileName.Extension.Equals(".docx", StringComparison.CurrentCultureIgnoreCase))
+                    //{
+                    //    Console.WriteLine("[Missed]");
+                    //    return;
+                    //}
+
+                    string fileToWriteTo = directory.FullName + "\\" + realBookFileName.Name;
+
+                    try
+                    {
+                        using (Stream streamToWriteTo = File.Open(fileToWriteTo, FileMode.Create))
+                        {
+                            streamToReadFrom.CopyTo(streamToWriteTo);
+                        }
+                        Console.WriteLine("[OK]");
+                    }
+                    catch (Exception ex)
+                    {
+                        LogEx(ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogEx(ex);
+            }
+
+        }
+
+        private static void LogEx(Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"ERROR: {ex.Message}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ ebookDownloader/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ebookDownloader
+{
+    class Program
+    {
+        public static string _directoryName => "downloadedPDF2";
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\tPDF downloader by Admin | ver: 1.0");
+            Console.ResetColor();
+
+            var link = "http://ligman.me/2sZVmcG";
+            //link = "http://ligman.me/1IW1oab";
+
+            var directory = new System.IO.DirectoryInfo(_directoryName);
+            directory.Create();
+            var files = directory.GetFiles();
+            //foreach (var file in files)
+            //    file.Delete();
+
+            var ebooksLinksListRaw = string.Empty;
+            var nd = new NetDownloader();
+
+            try
+            {
+                Console.Write("\nDownloading urls...\t");
+                ebooksLinksListRaw = nd.GetContent(new Uri(link)).Result;
+
+                if (string.IsNullOrEmpty(ebooksLinksListRaw))
+                    throw new Exception("Empty ebooks list!");
+
+                Console.WriteLine("[OK]");
+            }
+            catch (Exception ex)
+            {
+                PrintError(ex);
+                return;
+            }
+
+
+
+            var ebooksLinksList = ebooksLinksListRaw.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var ebooksUrisList = new List<Uri>();
+            foreach (var jakisLink in ebooksLinksList)
+            {
+                if(Uri.IsWellFormedUriString(jakisLink, UriKind.Absolute))
+                    ebooksUrisList.Add(new Uri(jakisLink));
+            }
+            Console.WriteLine($"--> Downloaded list of {ebooksUrisList.Count} ebooks");
+
+
+
+
+            Console.WriteLine("\nDownloading files started...");
+            try
+            {
+                var tasks = new List<Task>();
+                //Parallel.ForEach(ebooksUrisList, (ebook) => { NetDownloader.HttpGetFile(ebook, directory); });
+                foreach(var ebook in ebooksUrisList)
+                {
+                    nd.HttpGetFiles(ebook, directory);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                PrintError(ex);
+                return;
+            }
+
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nDownoading finished ! ! !\nPress any to exit . . .");
+            Console.ReadKey();
+        }
+
+        private static void PrintError(Exception ex)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("[ERROR] " + ex.Message);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
